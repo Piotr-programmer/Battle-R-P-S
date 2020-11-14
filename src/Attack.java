5eaff69303ff72733184587be503282a351ad498
Piotr-programmer <61473938+Piotr-programmer@users.noreply.github.com>
2020-11-14 15:53:01 +0100

    Update Attack.java
    questions

--- src/Attack.java
+++ src/Attack.java
@@ -1,11 +1,11 @@
 import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 
 public class Attack {
-    Type type;
+    Type type;  ////to jest obiekt type klasy Type?
 
 
     Attack(Type t) {
-        type = t;
+        type = t;  
     }
     Type getType(){
         return type;
